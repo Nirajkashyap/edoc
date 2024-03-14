cfa826d7e8951387ffcefc28221da3959f3eb6c1
Niraj <niraj38re@gmail.com>
2024-03-15 01:03:28 +0530

    update base path to use from env

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/edoc/",
+  base: process.env.VITE_BASE_PATH,
   plugins: [react()],
 });
